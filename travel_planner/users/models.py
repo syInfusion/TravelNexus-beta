d9f2874a0590cf41098b558080759b94cbb63a48
sh-enoch <wekesawafula.enoch@gmail.com>
2025-02-02 10:16:57 +0300

    functioning profile signal

--- travel_planner/users/models.py
+++ travel_planner/users/models.py
@@ -1,39 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.templatetags.static import static
 # Create your models here.
-from django.contrib.auth.models import  User
-from django.templatetags import static
-
 
 class Profile(models.Model):
-    """Define the user profile model."""
+    """Defines user profile class"""
     user =  models.OneToOneField(User, on_delete=models.CASCADE)
-    displayname =  models.CharField(max_length=20, null=True, blank=True)
-    profile_pic = models.ImageField(upload_to='avatars/', null=True, blank=True)
-    info = models.TextField(blank=True, null=True)
-
+    username =  models.CharField(max_length=12, null=True, blank=True)
+    user_pic = models.ImageField(upload_to="avatars/", null=True, blank=True)
+    info = models.TextField(null=True, blank=True, max_length=100)
+    
 
     def __str__(self):
         return str(self.user)
     
-
     @property
     def name(self):
-        """If user does not provide displayname."""
-        if self.displayname:
-            name = self.displayname
+        if self.username:
+            name = self.username
         else:
-            name =  self.user.username
+            name = self.user.username
 
         return name
     
 
     @property
-    def profile(self):
-        """Sets user profile to the default if the user profile has not been provided."""
+    def avatar(self):
+        """Defines a  method to assign a default avatar to a user."""
         try:
-            profile = self.profile_pic.url
+            avatar = self.user_pic.url
         except:
-            profile = static('static/avatar.svg')
+            avatar =  static('images/avatar.svg')
 
-        return profile
+        return avatar
---
+++ travel_planner/users/signals.py
@@ -0,0 +1,12 @@
+from .models import Profile
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save
+from django.contrib.auth.models import User
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """A post save signal to create a user profile anytime a user is created."""
+    if created:
+        Profile.objects.create(
+            user = instance
+        )
