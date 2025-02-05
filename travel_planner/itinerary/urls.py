8290bd56d1f426ec8430ac57b05cadf3f713ed47
mosetf <stanlym315@gmail.com>
2025-02-05 23:46:07 +0300

    Add URL patterns for home, profile, and logout views

--- travel_planner/itinerary/urls.py
+++ travel_planner/itinerary/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
+from .views import home, profile
 
 urlpatterns = [
-    
+    path('home/', home, name='home'),
+    path('profile/', profile, name='profile'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
