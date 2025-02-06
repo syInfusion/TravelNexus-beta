9ec08438c63899d0ede1803589e0d71bd9d0693a
mosetf <stanlym315@gmail.com>
2025-02-06 23:22:03 +0300

    Add logout and profile detail URLs to user routes

--- travel_planner/users/urls.py
+++ travel_planner/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import ProfileDetailView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns =  [
-    path('', home, name='home')
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('profile/', ProfileDetailView.as_view(), name='profile-detail')
 ]
