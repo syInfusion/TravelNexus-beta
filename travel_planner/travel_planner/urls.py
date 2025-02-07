379f5ec81157bc160f44e0b425b9de4d39e95611
mosetf <stanlym315@gmail.com>
2025-02-07 20:40:11 +0300

    Add landing page route to URL configuration

--- travel_planner/travel_planner/urls.py
+++ travel_planner/travel_planner/urls.py
@@ -19,11 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from users.views import landing_page
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
+    path('', landing_page, name='landing-page'),
     path("accounts/", include("allauth.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
     path('itinerary/', include('itinerary.urls'))
