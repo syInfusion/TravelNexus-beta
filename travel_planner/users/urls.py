6263dc621ece37645a8a12a68da5931ce5ff6c46
mosetf <stanlym315@gmail.com>
2025-02-07 13:49:17 +0300

    ``` No code changes made. ```

--- travel_planner/users/urls.py
+++ travel_planner/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProfileDetailView
+from .views import ProfileDetailView, profile_setup
 from django.contrib.auth.views import LogoutView
 
 urlpatterns =  [
+    path('profile-setup/', profile_setup, name='profile-setup'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('profile/', ProfileDetailView.as_view(), name='profile-detail')
 ]
