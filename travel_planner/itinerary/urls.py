3342b7e8f3ca251c769fe486ee53b78aefbe35f8
mosetf <stanlym315@gmail.com>
2025-02-15 23:55:53 +0300

    Add edit itinerary route to URL configuration

--- travel_planner/itinerary/urls.py
+++ travel_planner/itinerary/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import home, profile
-from .views import itinerary_view
+from .views import itinerary_view,  edit_itinerary_view
+from . import views
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('profile/', profile, name='profile'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('my-itinerary/', itinerary_view, name='my-itinerary'),
+    path("edit-itinerary/", edit_itinerary_view, name="edit_itinerary"),
+    path('itinerary/my-itinerary/', views.itinerary_view, name='itinerary_view'),
 ]
